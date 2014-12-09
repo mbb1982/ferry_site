2fb71002fdf6a2e2ae67cfba22c0e4bb785f23ef
mbb1982 <matthew@black1.org.uk>
2014-12-09 21:55:48 +0000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,18 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'nokogiri'
+require 'mechanize'
+agent = Mechanize.new
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+ferry_page = agent.get("http://ferrysite.dk/ferry.php?id=8502406&lang=en")
+name = ferry_page.search("center h2").inner_html
+item={:name=>name}
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+ferry_page.search("table tr").each{ |row|
+  key=row.children[0].inner_html
+  value=row.children[1].inner_html
+  item[key]=value
+}
+
+ScraperWiki::save_sqlite(item['IMO'],item,"ferries")
+
+
