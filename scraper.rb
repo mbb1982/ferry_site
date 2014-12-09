ff26a27a37a823fd02c02ec18c6c9eeb54bc453b
mbb1982 <matthew@black1.org.uk>
2014-12-09 22:22:55 +0000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -3,17 +3,21 @@ require 'nokogiri'
 require 'mechanize'
 agent = Mechanize.new
 
-ferry_page = agent.get("http://ferrysite.dk/ferry.php?id=8502406&lang=en")
-name = ferry_page.search("center h2").inner_html
-item={:name=>name}
+all_page = agent.get("http://ferrysite.dk/ferryname.php?")
 
-ferry_page.search("table tr").each{ |row|
-  key=row.children[0].inner_html
-  value=row.children[1].inner_html
-  unless ["Former names","Former owners","Sister ships","Notes"].include?(key)
-    key.gsub!(/ /,"_")
-    item[key]=value
-  end
+all_page.links_with(:href=>/ferry.php/).each{ |link|
+  ferry_page = link.click  #agent.get("http://ferrysite.dk/ferry.php?id=8502406&lang=en")
+  name = ferry_page.search("center h2").inner_html
+  item={:name=>name}
+  
+  ferry_page.search("table tr").each{ |row|
+    key=row.children[0].inner_html
+    value=row.children[1].inner_html
+    unless ["Former names","Former owners","Sister ships","Notes"].include?(key)
+      key.gsub!(/ /,"_")
+      item[key]=value
+    end
+  }
 }
 
 ScraperWiki::save_sqlite(['IMO'],item,"ferries")
